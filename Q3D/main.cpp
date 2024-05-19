c5508d8b0f5b39ecaacaacd7424f34e14c62a0ff
Japenus <2268809627@qq.com>
2024-05-19 13:48:38 +0800

    Publish QVision 1.0

--- Q3D/main.cpp
+++
@@ -1,9 +0,0 @@
-#include "q3d.h"
-#include <QApplication>
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    Q3D w;
-    w.show();
-    return a.exec();
-}
--- QVision/src/screencut.cpp
+++
@@ -1,5 +0,0 @@
-#include "screencut.h"
-ScreenCut::ScreenCut()
-{
-
-}
